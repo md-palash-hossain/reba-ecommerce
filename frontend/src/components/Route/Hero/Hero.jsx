f4e68cedfc02e8ba3cc63a4454c919a556e63a7f
md-palash-hossain <palashpol71@gmail.com>
2023-05-12 03:23:54 +0600

    Initial State

---
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../../styles/styles";
+
+const Hero = () => {
+  return (
+    <div
+      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
+      style={{
+        backgroundImage:
+          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
+      }}
+    >
+      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
+        <h1
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+        >
+         Discover Your Perfect Style
+        </h1>
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
+        Unleash Your Fashion Potential with Our Trendsetting Collections, Elevate Your Style to New Heights,<br/> and Make Every Outfit a Statement of Confidence and Individuality.
+        </p>
+        <Link to="/products" className="inline-block">
+            <div className={`${styles.button} my-5`}>
+                 <span className="text-[#fff] font-[Poppins] text-[18px]">
+                    Shop Now
+                 </span>
+            </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
